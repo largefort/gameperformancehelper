5ab16a7c747f1cf3b4d698654508abbf0ff5bacc
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-04-05 11:55:21 +0000

    Create gameperformancehelper.java

---
+++ gameperformancehelper.java
@@ -0,0 +1,33 @@
+var exec = require('cordova/exec');
+
+var PerformancePlugin = {
+    getFrameRate: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'getFrameRate', []);
+    },
+    getMemoryUsage: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'getMemoryUsage', []);
+    },
+    getCPUUsage: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'getCPUUsage', []);
+    },
+    getGPUUsage: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'getGPUUsage', []);
+    },
+    suggestOptimizations: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'suggestOptimizations', []);
+    },
+    startMonitoring: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'startMonitoring', []);
+    },
+    stopMonitoring: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'stopMonitoring', []);
+    },
+    enableOptimizationSuggestions: function(enabled, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'enableOptimizationSuggestions', [enabled]);
+    },
+    setOptimizationThresholds: function(cpuThreshold, gpuThreshold, fpsThreshold, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PerformancePlugin', 'setOptimizationThresholds', [cpuThreshold, gpuThreshold, fpsThreshold]);
+    }
+};
+
+module.exports = PerformancePlugin;
